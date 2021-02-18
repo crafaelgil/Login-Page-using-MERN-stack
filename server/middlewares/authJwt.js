0a96bb2971450ca85afe2fe450b8ef1cbae86ad1
Carlos Gil <crafaelgil@gmail.com>
2021-02-18 22:27:59 +0900

    Implement admin and moderator verification

--- server/middlewares/authJwt.js
+++ server/middlewares/authJwt.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/auth.config');
+const { user } = require('../models');
 const db = require('../models');
 
 const User = db.user;
@@ -20,3 +21,74 @@ verifyToken = (req, res, next) => {
     next();
   })
 }
+
+isAdmin = (req, res, next) => {
+  User
+    .findById(req.id)
+    .exec((err, user) => {
+      if(err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+
+      Role.find({
+        _id: { $in: user.roles }
+      },
+      (err, roles) => {
+        if(err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+
+        for (let i = 0; i < roles.length; i++) {
+          if(roles[i] === "admin") {
+            next();
+            return;
+          }
+        }
+
+        res.status(403).send({ message: "Admin role required!" });
+        return;
+      })
+    });
+}
+
+isModerator = (req, res, next) => {
+  User
+    .findById(req.id)
+    .exec((err, user) => {
+      if(err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+
+      Role.find({
+        _id: { $in: user.roles }
+      },
+      (err, roles) => {
+        if(err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+
+        for (let i = 0; i < roles.length; i++) {
+          if(roles[i] === "moderator") {
+            next();
+            return;
+          }
+        }
+
+        res.status(403).send({ message: "Moderator role required!" });
+        return;
+      })
+    });
+}
+
+const authJwt = {
+  verifyToken,
+  isAdmin,
+  isModerator
+};
+
+module.exports = authJwt;
+
