c1812831f2027e4ab9befb0691707de1a1e8d28e
Carlos Gil <crafaelgil@gmail.com>
2021-02-18 14:58:44 +0900

    Implement logic of App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import logo from './logo.svg';
 import './App.css';
 
+import AuthService from './services/auth.service';
+import BoardAdmin from './components/BoardAdmin';
+import BoardModerator from './components/BoardModerator';
+import BoardUser from './components/BoardUser';
+import Home from './components/Home';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Register from './components/Register';
+
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined
+    }
+  }
+
+  logOut() {
+    AuthService.logout();
+  }
+
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+
+    if(user) {
+      this.setState({
+        currentUser: user,
+        showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+        showModeratorBoard: user.roles.includes("ROLE_MODERATOR")
+      });
+    }
+  }
 }
 
 export default App;
