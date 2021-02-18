66fdf93e7f6a88f72c32694ac802ba5d48c01bd7
Carlos Gil <crafaelgil@gmail.com>
2021-02-18 21:40:27 +0900

    Implement checkRolesExiste

--- server/middlewares/verifySignUp.js
+++ server/middlewares/verifySignUp.js
@@ -40,3 +40,25 @@ checkDublicateUserOrEmail = (req, res, next) => {
         });
     });
 }
+
+checkRolesExisted = (req, res, next) => {
+  if(req.body.roles) {
+    for (let i = 0; i < req.body.length; i++) {
+      if(!ROLES.includes(req.body.roles[i])) {
+        res.status(400).send({
+          messsage: `Failed! Role ${req.body.roles[i]} does not exist!`
+        });
+        return;
+      }
+    }
+  }
+
+  next();
+}
+
+const verifySignUp = {
+  checkDublicateUserOrEmail,
+  checkRolesExisted
+};
+
+module.exports = verifySignUp;
