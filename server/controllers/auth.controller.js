5950db0dfd160800a7449fd40211947a9f6caf6c
Carlos Gil <crafaelgil@gmail.com>
2021-02-19 15:11:55 +0900

    Implement user signin API

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -62,5 +62,48 @@ exports.signup = (req, res) => {
 }
 
 exports.signin = (req, res) => {
+  User.findOne({
+    username: req.body.username
+  })
+    .populate("roles", "-__v")
+    .exec((err, user) => {
+      if(err) {
+        res.status(500).send({message: err});
+        return;
+      }
 
+      if(!user) {
+        return res.status(404).send({message: "User not found"});
+      }
+
+      var passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if(!passwordIsValid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: "Invalid password!"
+        });
+      }
+
+      var token = jwt.sign({ id: user.id }, config.secret, {
+        expiresIn: 86400
+      });
+
+      var authorities = [];
+
+      for (let i = 0; i < user.roles.length; i++) {
+        authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
+      }
+
+      res.status(200).send({
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        roles: authorities,
+        accessToken: token
+      });
+    });
 }
