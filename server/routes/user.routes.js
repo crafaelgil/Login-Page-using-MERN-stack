6cf15e808bea905db844093670d34567e293426c
Carlos Gil <crafaelgil@gmail.com>
2021-02-19 17:31:57 +0900

    Include complete user controller path

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,5 @@
 const { authJwt } = require('../middlewares');
-const controller = require('../controllers');
+const controller = require('../controllers/user.controller');
 
 module.exports = function(app) {
   app.use((req, res, next) => {
